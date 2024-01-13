bb2eec869ba076ff016fb2d7dac882a4f261eb96
Vishesh Agrawal <agrawalvishesh9271@gmail.com>
2024-01-13 12:07:23 +0530

    Chat Application

---
+++ src/main/java/com/websocket/geeksforgeeks/configuration/WebSocketConfig.java
@@ -0,0 +1,23 @@
+package com.websocket.geeksforgeeks.configuration;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.socket.config.annotation.EnableWebSocket;
+import org.springframework.web.socket.config.annotation.WebSocketConfigurer;
+import org.springframework.web.socket.config.annotation.WebSocketHandlerRegistry;
+
+import com.websocket.geeksforgeeks.handlers.SocketConnectionHandler;
+
+//This is configuration and thus all web socket connections will be handled by this class
+@Configuration
+@EnableWebSocket
+public class WebSocketConfig implements WebSocketConfigurer {
+
+	//Overriding a method which register the socket handlers into a Registry
+	@Override
+    public void registerWebSocketHandlers(WebSocketHandlerRegistry webSocketHandlerRegistry) {
+		//For adding a Handler we give the Handler class we created before with End point 
+		//Also we are managing the CORS policy for the handlers so that other domains can also access the socket
+        webSocketHandlerRegistry.addHandler(new SocketConnectionHandler(), "/hello").setAllowedOrigins("*");
+    }
+	
+}
